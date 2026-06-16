462396cbbf940636f2e245ec65407c68a3623bc7
abangpurwo-prog <abangpurwo@gmail.com>
2026-06-16 13:24:01 +0800

    Fix syntax error in users array

--- users.js
+++ users.js
@@ -1,6 +1,6 @@
 const users = [
     { username:"micola", password:"123456" },
     { username:"admin", password:"admin" },
-    { username:"Aminulwahib", password:"020487" }
+    { username:"Aminulwahib", password:"020487" },
     { username:"id.shopcenter", password:"00012" }
 ];
