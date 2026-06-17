ca6a8b2b79ccaab9a5a1920ec87d7c1706197e5f
abangpurwo-prog <abangpurwo@gmail.com>
2026-06-17 16:46:20 +0800

    Update users.js

--- users.js
+++ users.js
@@ -1,6 +1,7 @@
 const users = [
     { username:"micola", password:"123456" },
     { username:"admin", password:"admin78" },
+    { username:"Darussalam_Al_Hanafiyyah", password:"112233" },
     { username:"Aminulwahib", password:"020487" },
     { username:"id.shopcenter", password:"00012" }
 ];
