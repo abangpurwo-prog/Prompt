33d59769cbfe827564a075da5455f9378de09889
abangpurwo-prog <abangpurwo@gmail.com>
2026-06-20 17:25:38 +0800

    Update user list with a new username

--- users.js
+++ users.js
@@ -1,7 +1,7 @@
 const users = [
     { username:"Member01", password:"010101" },
     { username:"admin", password:"admin78" },
-    { username:"Darussalam_Al_Hanafiyyah", password:"112233" },
+    { username:"om.bagong", password:"112233" },
     { username:"Aminulwahib", password:"020487" },
     { username:"kabelmogami", password:"010101" },
     { username:"id.shopcenter", password:"00012" }
